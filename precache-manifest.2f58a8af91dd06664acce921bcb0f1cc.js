0153a756dcb1643d0aa6ac7133d94c2994be96ed
dimastad <dmitrii_portnov@mail.ru>
2020-10-02 08:09:22 +0400

    Updates

---
+++ precache-manifest.2f58a8af91dd06664acce921bcb0f1cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307066cf870bc7e7142a1dc119e52d2a",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "953bf326407f01376405",
+    "url": "/todo-list/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b90bea824ed5906cd20d",
+    "url": "/todo-list/static/css/main.70a06edf.chunk.css"
+  },
+  {
+    "revision": "953bf326407f01376405",
+    "url": "/todo-list/static/js/2.3ca168c2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo-list/static/js/2.3ca168c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b90bea824ed5906cd20d",
+    "url": "/todo-list/static/js/main.a96742d7.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
